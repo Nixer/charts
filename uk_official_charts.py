bf67a2e30de7510c1bec5674891ea5c1f4c2c251
Nikolai Shegai <nikolai_shegai@apple.com>
2018-12-22 22:00:06 -0800

    parser for UK charts added

--- uk_official_charts.py
+++ uk_official_charts.py
@@ -1,17 +1,34 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+import json
 
 
 def all_sundays(year):
+    """Return list of Sunday`s dates on particular year"""
     return pd.date_range(start=str(year), end=str(year+1), freq='W-SUN').strftime('%Y%m%d').tolist()
 
 
-for year in range(2018, 2019):
+charts = {}
+"""Write all UK Official charts for past 60 years to json file"""
+for year in range(1958, 2019):
     try:
         for date in all_sundays(year):
             r = requests.get(f"https://www.officialcharts.com/charts/singles-chart/{date}/7501/")
             soup = BeautifulSoup(r.text, 'html.parser')
-            print(soup.find("table", {"class": "chart-positions"}))
+            positions = soup.findAll("span", {"class": "position"})
+            titles = soup.findAll("div", {"class": "title"})
+            artists = soup.findAll("div", {"class": "artist"})
+            li = [
+                {
+                    "title": (titles[i].get_text()).strip().title(),
+                    "artist": (artists[i].get_text()).strip().title(),
+                    "rank": int((positions[i].get_text()).strip())
+                } for i in range(len(positions))
+            ]
+            charts[f"{date[:4]}-{date[4:6]}-{date[6:]}"] = li
     except:
         pass
+
+with open("uk_charts.json", "w+") as file:
+    file.write(json.dumps(charts))
